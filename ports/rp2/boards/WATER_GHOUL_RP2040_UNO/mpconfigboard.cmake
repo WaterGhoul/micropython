51e6328242f78f5be59af436afa20f52e92e91a3
WaterGhoul <dmikkelson17@gmail.com>
2023-11-30 14:32:29 -0600

    add WG UNO

---
+++ ports/rp2/boards/WATER_GHOUL_RP2040_UNO/mpconfigboard.cmake
@@ -0,0 +1,1 @@
+# cmake file for Water_Ghoul RP2040 UNO 16MB
---
+++ ports/rp2/boards/WATER_GHOUL_RP2040_UNO/mpconfigboard.h
@@ -0,0 +1,19 @@
+// https://linktr.ee/water_ghoul
+
+#define MICROPY_HW_BOARD_NAME          "Water_Ghoul RP2040 UNO 16MB"
+#define MICROPY_HW_FLASH_STORAGE_BYTES (15 * 1024 * 1024)
+
+
+#define MICROPY_HW_UART1_TX  (8)
+#define MICROPY_HW_UART1_RX  (9)
+#define MICROPY_HW_UART1_CTS (10)
+#define MICROPY_HW_UART1_RTS (11)
+
+#define MICROPY_HW_I2C0_SCL  (17)
+#define MICROPY_HW_I2C0_SDA  (16)
+
+// User LED GPIO13
+// Neopixel Data on GPIO14
+
+
+
